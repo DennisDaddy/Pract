d21415914c4bbc462655b2adb63ab4dffc5fc293
DennisDaddy <softteez95@gmail.com>
2017-01-06 12:40:28 +0300

    add classes

--- animal.rb
+++ animal.rb
@@ -1,11 +1,36 @@
 class Bird
+	def talk
+		puts " chirp! chirp!"
+		
+	end
+
+	def move(destination)
+		puts "flying to the #{destination}."
+		
+	end
 
 end
 
 class Dog
+	def talk
+		puts "Bark"
+		
+	end
 	
+	def move(destination)
+		puts "running to the #{destination}."
+		
+	end
 end
 
 class Cat
+	def talk
+		puts "Meow"
+		
+	end
 	
+	def move(destination)
+		puts "running to the #{destination}."
+		
+	end
 end
